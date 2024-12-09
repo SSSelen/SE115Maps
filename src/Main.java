5d9d27141f55a994aec4eb02d2900d4c76502002
Selen Altınsoy <selen.altinsoy@std.izmirekonomi.edu.tr>
2024-12-09 23:30:00 +0300

    deneme 3

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("deneme");
         System.out.println("deneme2");
         System.out.println("deneme3");
+        System.out.println("deneme4");
         }
     }
