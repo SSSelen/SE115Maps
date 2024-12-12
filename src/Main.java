9e7d366c2639ba4724e19eaa75788e1cb4fbf07b
Selen Altınsoy <selen.altinsoy@std.izmirekonomi.edu.tr>
2024-12-12 15:59:01 +0300

    file read

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+import java.nio.file.Paths;
+import java.io.IOException;
+//import java.io.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("deneme");
-        System.out.println("deneme2");
-        System.out.println("deneme3");
-        System.out.println("deneme4");
-        System.out.println("deneme5");
+        try {
+            Scanner reader = new Scanner(Paths.get("deneme.txt"));
+            System.out.println("File read is successful!");
+        } (IOException x){
+            System.out.println("<File line number> <Error description>" + x.getFile());
         }
     }
+}
